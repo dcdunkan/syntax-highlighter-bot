47236df7e970eb78419c0073b3a1f33bf04f6235
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-04-15 19:04:11 +0530

    Fix: Load ENV vars from ENV

--- src/bot/env.ts
+++ src/bot/env.ts
@@ -1,6 +1,7 @@
 import { cleanEnv, config, str } from "../deps.ts";
+await config();
 
-export default cleanEnv(await config(), {
+export default cleanEnv(Deno.env.toObject(), {
   BOT_TOKEN: str(),
   DETA_KEY: str(),
 });
