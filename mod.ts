813765496e8be7cd1d66b266b68d7b0ebda0220c
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-31 19:14:08 +0530

    feat(webhook): redirect on non-update requests

--- mod.ts
+++ mod.ts
@@ -6,15 +6,16 @@ const handleUpdate = webhookCallback(bot, "std/http");
 const port = parseInt(Deno.env.get("PORT") ?? "8000");
 console.log(`Listening on: ${port}`);
 
+await bot.init();
+
 serve(async (req) => {
   if (req.method == "POST") {
     try {
       return await handleUpdate(req);
     } catch (err) {
       console.error(err);
-      return new Response();
     }
   }
 
-  return new Response();
+  return Response.redirect(`https://telegram.me/${bot.botInfo.username}`);
 }, { port });
