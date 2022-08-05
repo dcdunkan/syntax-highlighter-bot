0e18f405f8134ae40b2a87eb1ff275149493ac5e
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-08-05 15:04:38 +0530

    fix: double logs

--- mod.ts
+++ mod.ts
@@ -3,9 +3,6 @@ import { bot } from "./src/bot/bot.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
-const port = parseInt(Deno.env.get("PORT") ?? "8000");
-console.log(`Listening on: ${port}`);
-
 await bot.init();
 
 serve(async (req) => {
@@ -18,4 +15,4 @@ serve(async (req) => {
   }
 
   return Response.redirect(`https://telegram.me/${bot.botInfo.username}`);
-}, { port });
+});
