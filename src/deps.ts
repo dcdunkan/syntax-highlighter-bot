d6fdec72bd44bfb1be03397a3433c1fe0f8a3a0e
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-08-05 19:36:55 +0530

    chore: group deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,20 +1,14 @@
-// src/core
 import { PublicApi } from "./types.ts";
-import hljs from "https://unpkg.com/@highlightjs/cdn-assets@11.5.1/es/highlight.min.js";
-export default hljs as PublicApi;
-export { parse as parseCSS } from "https://deno.land/x/css@0.3.0/mod.ts";
-export {
-  DOMParser,
-  HTMLElement,
-  HTMLTableCellElement,
-} from "https://esm.sh/linkedom@0.14.12";
-export { createSvg2png, initialize } from "https://esm.sh/svg2png-wasm@1.3.4";
 
-// src/bot
-export { cleanEnv, str } from "https://deno.land/x/envalid@v0.0.3/mod.ts";
+// std/
+export { prettyBytes as bytes } from "https://deno.land/std@0.151.0/fmt/bytes.ts";
 export { config } from "https://deno.land/std@0.151.0/dotenv/mod.ts";
 export { resolve } from "https://deno.land/std@0.151.0/path/mod.ts";
+export { serve } from "https://deno.land/std@0.151.0/http/mod.ts";
 
+// x/
+export { parse as parseCSS } from "https://deno.land/x/css@0.3.0/mod.ts";
+export { cleanEnv, str } from "https://deno.land/x/envalid@v0.0.3/mod.ts";
 export {
   Bot,
   Composer,
@@ -32,13 +26,22 @@ export {
   type Message,
   type MessageEntity,
 } from "https://deno.land/x/grammy@v1.10.1/types.ts";
-
 export { DetaAdapter } from "https://deno.land/x/grammy_storages@v2.0.0/deta/src/mod.ts";
 
+// ghc.deno.dev/
 export {
   I18n,
   type I18nFlavor,
 } from "https://ghc.deno.dev/dcdunkan/i18n-2.0@2.0/src/mod.ts";
 
-export { prettyBytes as bytes } from "https://deno.land/std@0.151.0/fmt/bytes.ts";
-export { serve } from "https://deno.land/std@0.151.0/http/server.ts";
+// esm.sh/
+export {
+  DOMParser,
+  HTMLElement,
+  HTMLTableCellElement,
+} from "https://esm.sh/linkedom@0.14.12";
+export { createSvg2png, initialize } from "https://esm.sh/svg2png-wasm@1.3.4";
+
+// unpkg.com/
+import hljs from "https://unpkg.com/@highlightjs/cdn-assets@11.5.1/es/highlight.min.js";
+export default hljs as PublicApi;
